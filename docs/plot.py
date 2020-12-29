8bd5cea61b929bd25db6de6f6b028502cfe6e1d7
Askill <ofjokg@gmail.com>
2020-12-29 14:08:37 +0100

    smol change in nameing

--- docs/plot.py
+++ docs/plot.py
@@ -4,10 +4,12 @@ import pandas as pd
 import os
 
 data = pd.read_csv(os.path.join(os.path.dirname(__file__), "bm.csv")) 
-data = data.drop(columns=['total'])
-data = data.set_index(["ce_average_threads", "ce_comp_threads", "lf_threads", "videoBufferLength"])
+data = data.drop(columns=['Time in Total'])
+data = data.set_index(["ContourExtractor Threads for Avg", "ContourExtractor Threads for comp", "LayerFactory Threads", "Video Buffer Length"])
 
 labels = [f"{row[0]}, {row[1]}, {row[2]}, {row[3]}" for row in data.values]
-data.plot.barh(stacked=True, label=labels)
+data[:-12].plot.barh(stacked=True, label=labels)
+
+#data.groupby(["ContourExtractor Threads for Avg"]).plot(kind="barh", stacked=True,label =labels)
+plt.xlabel("time in seconds")
 plt.show()
-print(data)
